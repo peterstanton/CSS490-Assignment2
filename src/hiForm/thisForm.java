08f6ad0c932f9c45c962cb155abf8c7890762e4e
Peter Stanton <stantp2@uw.edu>
2018-01-28 20:37:45 -0800

    tweak.

--- src/hiForm/thisForm.java
+++ src/hiForm/thisForm.java
@@ -18,6 +18,8 @@ import com.google.gson.*;
 public class thisForm {
     private static final String baseGeocode ="https://maps.googleapis.com/maps/api/geocode/json?address=";
     private static final String googleAPI ="AIzaSyAXOUJZME49BZfMWw3XGCqdrZ0-2MvQj6U";
+    private static final String baseWeather = "https://api.darksky.net/forecast/";
+    private static final String weatherKey ="6695e2cfcb5944be3b28b2bcbe4cce57";
     public static void main(String[] args) {
         JFrame frame = new JFrame("thisForm");
         frame.setContentPane(new thisForm().mainPanel);
@@ -35,16 +37,17 @@ public class thisForm {
 
     private void process(String input) {
         String[] parsed = input.split("\\W+");
-        URL googleURL = processURL(parsed);
-        String geoResult = processGeoResponse(googleURL);
+        URL googleURL = processGeoURL(parsed);
+        String geoResult = processResponse(googleURL);
         String[] geoParsed = geoResult.split("\n");
         double geoLat = Double.parseDouble(geoResult.split("\n")[63].split(":")[1].replaceAll(",", ""));
         double geoLong = Double.parseDouble((geoResult.split("\n")[64].split(":")[1]).replaceAll(",", ""));
+        URL darkSkyURL = processWeatherURL(geoLat, geoLong);
+        String weatherResult = processResponse(darkSkyURL);
         String temp = new String();
-
-
     }
-    private URL processURL(String[] parsed) {
+
+    private URL processGeoURL(String[] parsed) {
         String joined = String.join("+", parsed);
         joined += "&key=" + googleAPI;
         try {
@@ -57,20 +60,19 @@ public class thisForm {
         return null;
     }
 
-    private String processGeoResponse(URL googleURL) {
+    private String processResponse(URL inURL) {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(googleURL.openStream()));
-            String geoResult = new String();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inURL.openStream()));
+            String outResult = new String();
             StringBuffer buffer = new StringBuffer();
             int read;
-
             char[] chars = new char[1024];
             try {
                 while ((read = reader.read(chars)) != -1)
                     buffer.append(chars, 0, read);
 
-                geoResult = buffer.toString();
-                return geoResult;
+                outResult = buffer.toString();
+                return outResult;
             }
             catch(IOException e) {
                 //blah
@@ -81,6 +83,18 @@ public class thisForm {
         }
         return null;
     }
+
+    private URL processWeatherURL(double geoLat, double geoLong) {
+        String joined = baseWeather + weatherKey + '/' + geoLat + ',' + geoLong;
+        try {
+            return new URL(joined);
+        }
+        catch(MalformedURLException e) {
+            //blah
+        }
+        return null;
+    }
+
 
     //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 
